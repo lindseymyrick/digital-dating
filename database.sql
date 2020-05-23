657f425201ed936df32ab607c01da53978579840
Lindsey Myrick <lindsey.k.myrick@gmail.com>
2020-05-23 09:31:09 -0500

    database and tables created

--- database.sql
+++ database.sql
@@ -8,3 +8,29 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "favorite_drink" (
+    "id" INT UNIQUE NOT NULL, 
+    "drink_name" VARCHAR (50),
+    "image_url" VARCHAR (100), 
+    "directions" VARCHAR (500), 
+    "comments" VARCHAR (500)
+);
+
+CREATE TABLE "ingredients" (
+    "id" SERIAL PRIMARY KEY, 
+    "ingredient_name" VARCHAR (50), 
+
+);
+
+CREATE TABLE "favorite_drink_ingredients" (
+    "id" SERIAL PRIMARY KEY, 
+    "recipe_id" INT, 
+    "ingredient_id" INT
+
+)
+
+CREATE TABLE "deleted_drinks" (
+    "id" INTEGER, 
+    "drink_name" VARCHAR (50)
+)
