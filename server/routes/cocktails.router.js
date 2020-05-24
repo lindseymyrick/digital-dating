2f3a3d1a2ff915eb0627ba10fadd11c8f1ef2347
Lindsey Myrick <lindsey.k.myrick@gmail.com>
2020-05-23 21:12:34 -0500

    api request working

--- server/routes/cocktails.router.js
+++ server/routes/cocktails.router.js
@@ -6,21 +6,20 @@ require('dotenv').config();
 
 
 router.get('/:searchTerm', (req, res) => {
-    console.log(' in /search GET', req.params);
-    let searchTerm = req.params;
-    console.log('API KEY', process.env.GIPHY_API_KEY);
-    // axios.get(`https://www.thecocktaildb.com/api/json/v1/${process.env.COCKTAIL_DB_API_KEY}filter.php?i=${searchTerm}`)
-    //     .then(response => {
-    //         console.log('API DATA',response.data)
-    //         // res.send(response.data);
-    //     }).catch(error => {
-    //         console.log('Error adding giphy url', error)
-    //     })
+    console.log(' in /search GET', req.params.searchTerm);
+    let searchTerm = req.params.searchTerm;
+    // console.log(`https://www.thecocktaildb.com/api/json/v1/${process.env.COCKTAIL_DB_API_KEY}filter.php?i=${searchTerm}`)
+    console.log('API KEY', process.env.COCKTAIL_DB_API_KEY);
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/${process.env.COCKTAIL_DB_API_KEY}/filter.php?i=${searchTerm}`)
+        .then(response => {
+            console.log('API DATA',response.data)
+            // res.send(response.data);
+        }).catch(error => {
+            console.log('Error adding giphy url', error)
+        })
 });
 
-// https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
-// https://www.thecocktaildb.com/api/json/v1/undefinedfilter.php?i=
-
-// COCKTAIL_DB_API_KEY
+// https://www.thecocktaildb.com/api/json/v1/1;filter.php?i=Vodka
+// https://www.thecocktaildb.com/api/json/v1/1;filter.php?i=Vodka
 
 module.exports = router;
