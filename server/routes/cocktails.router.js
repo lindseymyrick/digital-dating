34e5eb46666d71f2ff56e7d18009ebdab001c67f
Lindsey Myrick <lindsey.k.myrick@gmail.com>
2020-05-23 20:52:26 -0500

    .env working

--- server/routes/cocktails.router.js
+++ server/routes/cocktails.router.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool');
+const axios = require('axios');
+require('dotenv').config();
+
+
 
 router.get('/:searchTerm', (req, res) => {
     console.log(' in /search GET', req.params);
-    // let searchTerm = req.body;
-    // axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${searchTerm}&limit=9&rating=pg&lang=en`)
+    let searchTerm = req.params;
+    console.log('API KEY', process.env.GIPHY_API_KEY);
+    // axios.get(`https://www.thecocktaildb.com/api/json/v1/${process.env.COCKTAIL_DB_API_KEY}filter.php?i=${searchTerm}`)
     //     .then(response => {
-    //         res.send(response.data);
+    //         console.log('API DATA',response.data)
+    //         // res.send(response.data);
     //     }).catch(error => {
     //         console.log('Error adding giphy url', error)
     //     })
 });
 
+// https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
+// https://www.thecocktaildb.com/api/json/v1/undefinedfilter.php?i=
 
-
-
+// COCKTAIL_DB_API_KEY
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 require('dotenv').config();
 
+
 const app = express();
 const bodyParser = require('body-parser');
 const sessionMiddleware = require('./modules/session-middleware');
