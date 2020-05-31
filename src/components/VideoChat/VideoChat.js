4286cf2a94cce05353a000ab8ba7184dba17aa85
Lindsey Myrick <lindsey.k.myrick@gmail.com>
2020-05-31 11:59:17 -0500

    videochat draggable

--- src/components/VideoChat/VideoChat.js
+++ src/components/VideoChat/VideoChat.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import DailyIframe from '@daily-co/daily-js';
 import Draggable from 'react-draggable'; 
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
+import styles from '../ui/Theme';
+import { withStyles } from '@material-ui/core/styles';
+
 
 export class VideoChat extends Component {
     constructor(props) {
@@ -16,17 +21,54 @@ export class VideoChat extends Component {
         this.daily.join({ url: this.props.url });
     }
 
-    eventLogger = (e: MouseEvent, data: Object) => {
-        console.log('Event: ', e);
-        console.log('Data: ', data);
+    state = {
+        activeDrags: 0,
+        deltaPosition: {
+            x: 0, y: 0
+        },
+        controlledPosition: {
+            x: -400, y: 200
+        }
     };
+
+    handleDrag = (e, ui) => {
+        const { x, y } = this.state.deltaPosition;
+        this.setState({
+            deltaPosition: {
+                x: x + ui.deltaX,
+                y: y + ui.deltaY,
+            }
+        });
+    };
+
+    onStart = () => {
+        this.setState({ activeDrags: ++this.state.activeDrags });
+    };
+
+    onStop = () => {
+        this.setState({ activeDrags: --this.state.activeDrags });
+    };
+
     render() {
-        
-        return <iframe className="Video-Frame"
+        const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
+        const { deltaPosition, controlledPosition } = this.state;
+        const { classes } = this.props; //need this for cards 
+        return( 
+            <>
+         <Grid container direction="row" className={classes.gridRoot} alignItems="top" spacing={2}> 
+        <Draggable {...dragHandlers}>
+            <div className= "Video-Box">
+        <iframe className="Video-Frame"
             title="video call iframe"
             ref={this.iframeRef}
             allow="camera; microphone; fullscreen"
         ></iframe>
+            </div>
+            </Draggable>
+            </Grid>
+            </>
+
+        )
     }
 }
-export default VideoChat;
+export default (withStyles(styles)(VideoChat)); ;
